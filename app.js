6c224a288f7ab4bdb5db7af87379f5e9e1f01494
lukeanders70 <luke.larson@berkeley.edu>
2018-04-22 12:45:50 -0700

    multiple spheres, different direcrions

--- app.js
+++ app.js
@@ -1,3 +1,94 @@
+var make_sphere = function(r, center, triangleVertices, num_lat, num_lon){
+
+	var num_vertices = 0
+
+	for (let lat = 1.0; lat <= num_lat + 1.0; ++lat) {
+		for (let lon = 0.0; lon < num_lon; ++lon) {
+			let a1 = (lat / (num_lat + 1)) * Math.PI; //radians away from straight up
+			let a2 = (lon / (num_lon)) * Math.PI * 2.0; //radians aorund circle
+
+			let x00 = center[0] + (r * Math.sin(a2) * Math.sin(a1));
+			let y00 = center[1] + (r * Math.cos(a1));
+			let z00 = center[2] + (r * Math.sin(a1) * Math.cos(a2));
+
+
+			let a3 = (lat / (num_lat + 1)) * Math.PI; //radians away from straight up
+			let a4 = ((lon + 1.0)/ (num_lon)) * Math.PI * 2.0; //radians aorund circle
+
+			let x10 = center[0] + (r * Math.sin(a4) * Math.sin(a3));
+			let y10 = center[1] + (r * Math.cos(a3));
+			let z10 = center[2] + (r * Math.sin(a3) * Math.cos(a4));
+
+
+			let a5 = ((lat-1.0) / (num_lat + 1)) * Math.PI; //radians away from straight up
+			let a6 = ((lon + 1.0)/ (num_lon)) * Math.PI * 2.0; //radians aorund circle
+
+			let x11 = center[0] + (r * Math.sin(a6) * Math.sin(a5));
+			let y11 = center[1] + (r * Math.cos(a5));
+			let z11 = center[2] + (r * Math.sin(a5) * Math.cos(a6));
+
+
+			let a7 = ((lat-1.0) / (num_lat + 1)) * Math.PI; //radians away from straight up
+			let a8 = ((lon)/ (num_lon)) * Math.PI * 2.0; //radians aorund circle
+
+			let x01 = center[0] + (r * Math.sin(a8) * Math.sin(a7));
+			let y01 = center[1] + (r * Math.cos(a7));
+			let z01 = center[2] + (r * Math.sin(a7) * Math.cos(a8));
+
+			triangleVertices.push(x00)
+			triangleVertices.push(y00)
+			triangleVertices.push(z00)
+			triangleVertices.push(1.0)
+			triangleVertices.push(1.0)
+			triangleVertices.push(0.0)
+
+			triangleVertices.push(x10)
+			triangleVertices.push(y10)
+			triangleVertices.push(z10)
+			triangleVertices.push(0.7)
+			triangleVertices.push(0.0)
+			triangleVertices.push(0.3)
+
+
+			triangleVertices.push(x01)
+			triangleVertices.push(y01)
+			triangleVertices.push(z01)
+			triangleVertices.push(1.0)
+			triangleVertices.push(0.0)
+			triangleVertices.push(0.0)
+
+
+			triangleVertices.push(x10)
+			triangleVertices.push(y10)
+			triangleVertices.push(z10)
+			triangleVertices.push(0.0)
+			triangleVertices.push(1.0)
+			triangleVertices.push(0.0)
+
+
+			triangleVertices.push(x11)
+			triangleVertices.push(y11)
+			triangleVertices.push(z11)
+			triangleVertices.push(0.0)
+			triangleVertices.push(0.0)
+			triangleVertices.push(1.0)
+
+
+			triangleVertices.push(x01)
+			triangleVertices.push(y01)
+			triangleVertices.push(z01)
+			triangleVertices.push(0.0)
+			triangleVertices.push(0.3)
+			triangleVertices.push(0.7)
+
+			num_vertices += 6;
+
+		}
+	}
+
+	return num_vertices;
+};
+
 var InitDemo = function(){
 
 	var canvas = document.getElementById('render_canvas');
@@ -51,103 +142,11 @@ var InitDemo = function(){
 	//
 	// create buffer
 	//
-
 	var triangleVertices = [];
-
-	var num_lat = 8.0;
-	var num_lon = 16.0;
-	var center = [0.0, 0.0, 0.0];
-	var vertices = [];
-	var r = 1.0;
-
-	for (let lat = 1.0; lat <= num_lat + 1.0; ++lat) {
-		for (let lon = 0.0; lon < num_lon; ++lon) {
-			let a1 = (lat / (num_lat + 1)) * Math.PI; //radians away from straight up
-			let a2 = (lon / (num_lon)) * Math.PI * 2.0; //radians aorund circle
-
-			let x00 = center[0] + (r * Math.sin(a2) * Math.sin(a1));
-			let y00 = center[1] + (r * Math.cos(a1));
-			let z00 = center[2] + (r * Math.sin(a1) * Math.cos(a2));
+	var num_vertices_s1 = make_sphere(.5, [0.5,0.0,0.0], triangleVertices, 8.0, 16.0);
+	var num_vertices_s2 = make_sphere(.25, [-0.5,0.0,0.0], triangleVertices, 8.0, 8.0);
 
 
-			let a3 = (lat / (num_lat + 1)) * Math.PI; //radians away from straight up
-			let a4 = ((lon + 1.0)/ (num_lon)) * Math.PI * 2.0; //radians aorund circle
-
-			let x10 = center[0] + (r * Math.sin(a4) * Math.sin(a3));
-			let y10 = center[1] + (r * Math.cos(a3));
-			let z10 = center[2] + (r * Math.sin(a3) * Math.cos(a4));
-
-
-			let a5 = ((lat-1.0) / (num_lat + 1)) * Math.PI; //radians away from straight up
-			let a6 = ((lon + 1.0)/ (num_lon)) * Math.PI * 2.0; //radians aorund circle
-
-			let x11 = center[0] + (r * Math.sin(a6) * Math.sin(a5));
-			let y11 = center[1] + (r * Math.cos(a5));
-			let z11 = center[2] + (r * Math.sin(a5) * Math.cos(a6));
-
-
-			let a7 = ((lat-1.0) / (num_lat + 1)) * Math.PI; //radians away from straight up
-			let a8 = ((lon)/ (num_lon)) * Math.PI * 2.0; //radians aorund circle
-
-			let x01 = center[0] + (r * Math.sin(a8) * Math.sin(a7));
-			let y01 = center[1] + (r * Math.cos(a7));
-			let z01 = center[2] + (r * Math.sin(a7) * Math.cos(a8));
-
-/*					console.log(a1, a2, a3, a4, a5, a6, a7, a8);
-			console.log(x00, y00, z00);
-			console.log(x10, y10, z10);
-			console.log(x11, y11, z11);
-			console.log(x01, y01, z01);*/
-
-
-			triangleVertices.push(x00)
-			triangleVertices.push(y00)
-			triangleVertices.push(z00)
-			triangleVertices.push(1.0)
-			triangleVertices.push(1.0)
-			triangleVertices.push(0.0)
-
-			triangleVertices.push(x10)
-			triangleVertices.push(y10)
-			triangleVertices.push(z10)
-			triangleVertices.push(0.7)
-			triangleVertices.push(0.0)
-			triangleVertices.push(0.3)
-
-
-			triangleVertices.push(x01)
-			triangleVertices.push(y01)
-			triangleVertices.push(z01)
-			triangleVertices.push(1.0)
-			triangleVertices.push(0.0)
-			triangleVertices.push(0.0)
-
-
-			triangleVertices.push(x10)
-			triangleVertices.push(y10)
-			triangleVertices.push(z10)
-			triangleVertices.push(0.0)
-			triangleVertices.push(1.0)
-			triangleVertices.push(0.0)
-
-
-			triangleVertices.push(x11)
-			triangleVertices.push(y11)
-			triangleVertices.push(z11)
-			triangleVertices.push(0.0)
-			triangleVertices.push(0.0)
-			triangleVertices.push(1.0)
-
-
-			triangleVertices.push(x01)
-			triangleVertices.push(y01)
-			triangleVertices.push(z01)
-			triangleVertices.push(0.0)
-			triangleVertices.push(0.3)
-			triangleVertices.push(0.7)
-
-		}
-	}
 
 /*	var triangleVertices = 
 	[ // X  ,  Y  ,  Z     R,   G,   B
@@ -203,7 +202,7 @@ var InitDemo = function(){
 	var projection_matrix = new Float32Array(16);
 	mat4.identity(world_matrix);
 	mat4.lookAt(view_matrix, [0,-2,-5], [0,0,0], [0,1,0]); // camera: location, position looking at, direction, that it up
-	mat4.perspective(projection_matrix, glMatrix.toRadian(45), canvas.clientWidth/canvas.clientHeight, 0.1, 1000.0) // fov in rad, aspect ratio width/height, near plane and far plane; 
+	mat4.perspective(projection_matrix, glMatrix.toRadian(45), canvas.clientWidth/canvas.clientHeight, 0.1, 1000.0); // fov in rad, aspect ratio width/height, near plane and far plane; 
 
 	//sending to shader
 	gl.uniformMatrix4fv(world_uniform_location, gl.FALSE, world_matrix); //uniform matrix of floats. gl.FALSE just indicates that we don't want the transpose
@@ -218,15 +217,23 @@ var InitDemo = function(){
 	var angle = 0;
 	gl.enable(gl.DEPTH_TEST);
 	var loop = function() {
+
+		gl.clearColor(0.0, 0.0, 0.0, 1.0);
+		gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+
 		//		mil Math.since start   to secs     one full rotation per six seconds 
 		angle = performance.now() / 1000     / 6 * 2 * Math.PI;
+
 		//			input         original matrix  angle   axis
 		mat4.rotate(world_matrix, identity_matrix, angle, [0,1,0]);
 		gl.uniformMatrix4fv(world_uniform_location, gl.FALSE, world_matrix);
 
-		gl.clearColor(0.0, 0.0, 0.0, 1.0);
-		gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
-		gl.drawArrays(gl.TRIANGLES, 0 , 864);
+		gl.drawArrays(gl.TRIANGLES, 0 , num_vertices_s1);
+
+		mat4.rotate(world_matrix, identity_matrix, angle, [0,1,0]);
+		gl.uniformMatrix4fv(world_uniform_location, gl.FALSE, world_matrix);
+
+		gl.drawArrays(gl.TRIANGLES, num_vertices_s1 , num_vertices_s2);
 
 		requestAnimationFrame(loop)
 	};
